de0889855d4854709fb387d7b30a21283885bcb0
Allan Febrian Diavanda <allanfebriandiavanda@gmail.com>
2023-09-05 18:30:00 +0700

    Home changes

--- src/components/Spline.js
+++
@@ -1,9 +0,0 @@
-
-import { Application } from '@splinetool/runtime';
-
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-app.load('https://prod.spline.design/ytmXzYtUkP-8gsQF/scene.splinecode');
-
-
-
